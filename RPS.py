d7c28149a58392fd82470c1a770fe0323dce1d78
ksm5062010 <ksm5062010@gmail.com>
2025-07-24 22:56:22 +1000

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,1 +1,16 @@
+from collections import Counter
+
 def player(prev_play, opponent_history=[]):
+    if prev_play:
+        opponent_history.append(prev_play)
+    count = Counter(opponent_history)
+    if count:
+        most_common_move = count.most_common(1)[0][0]
+    else:
+        most_common_move = "R"  # Default guess for first move
+    winning_move = {
+        "R": "P",  # Paper beats Rock
+        "P": "S",  # Scissors beats Paper
+        "S": "R"   # Rock beats Scissors
+    }
+    return winning_move[most_common_move]
