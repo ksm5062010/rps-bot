562f2996890f6d29521aed8c0003334d4ecdcd6d
ksm5062010 <ksm5062010@gmail.com>
2025-07-24 22:56:57 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from RPS import player
+
+import RPS_game
+
+# A simple random bot to play against
+import random
+def random_bot(prev_play, opponent_history=[]):
+    return random.choice(["R", "P", "S"])
+
+if __name__ == "__main__":
+    # Play 1000 games between your player and random_bot
+    RPS_game.play(player, random_bot, num_games=1000, verbose=False)
 
